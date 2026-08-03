69e39868a9999325759c1b13f64ea1c11f357194
Ralf Pereira (Piredda) <ralfpiredda@gmail.com>
2026-08-03 02:19:33 -0300

    Update zowe_operations.sh

--- .github/scripts/zowe_operations.sh
+++ .github/scripts/zowe_operations.sh
@@ -5,11 +5,12 @@ set -e
 
 # Convert username to lowercase
 LOWERCASE_USERNAME=$(echo "$ZOWE_USERNAME" | tr '[:upper:]' '[:lower:]')
+PARAM_STRING="-H $HOSTNAME -P 10443 -u $ZOWE_USERNAME --pw $ZOWE_PASSWORD --ru false"
 
 # Check if directory exists, create if it doesn't
 if ! zowe zos-files list uss-files "/z/$LOWERCASE_USERNAME/cobolcheck" &>/dev/null; then
   echo "Directory does not exist. Creating it..."
-  zowe zos-files create uss-directory "/z/$LOWERCASE_USERNAME/cobolcheck"
+  zowe zos-files create uss-directory "/z/$LOWERCASE_USERNAME/cobolcheck" $PARAM_STRING
 else
   echo "Directory already exists."
 fi
@@ -17,8 +18,8 @@ fi
 # Upload files
 zowe zos-files upload dir-to-uss "./cobol-check" "/z/$LOWERCASE_USERNAME/cobolcheck" \
   --recursive \
-  --binary-files "cobol-check-0.2.9.jar"
+  --binary-files "cobol-check-0.2.9.jar" $PARAM_STRING 
 
 # Verify upload
 echo "Verifying upload:"
-zowe zos-files list uss-files "/z/$LOWERCASE_USERNAME/cobolcheck"
+zowe zos-files list uss-files "/z/$LOWERCASE_USERNAME/cobolcheck" $PARAM_STRING 
